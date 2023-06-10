1ccd21821fcbc9dd0c291ed75ee7e8d6551cb362
Timofey Shichalin <shichatim@gmail.com>
2023-06-10 04:34:17 -0400

    map PUT route

--- backend/routes/circuit_map_routes.ts
+++ backend/routes/circuit_map_routes.ts
@@ -1,6 +1,7 @@
 import {FastifyInstance} from "fastify";
 import {CircuitMap} from "../src/db/entities/CircuitMap.js";
 import { User } from "../src/db/entities/User.js";
+import { wrap } from '@mikro-orm/core';
 
 export function CircuitMapRoutesInit(app: FastifyInstance) {
 	// Route to create a map for a user
@@ -23,4 +24,22 @@ export function CircuitMapRoutesInit(app: FastifyInstance) {
 			reply.status(500).send(err);
 		}
 	});
+	
+	// Modify an existing map
+	app.put("/circuitmaps", async (req, reply) => {
+		// @ts-ignore
+		let { email, mapTitle, newMap } = req.body;
+		// Modify the map
+		try {
+			const user = await req.em.findOne(User, {email: email});
+			const map = await req.em.findOne(CircuitMap, {
+				owner: user, title: mapTitle
+			});
+			await wrap(map).assign({ circuitMap: newMap });
+			await req.em.flush();
+			return reply.send(map);
+		} catch (err) {
+			reply.status(500).send(err);
+		}
+	})
 }
