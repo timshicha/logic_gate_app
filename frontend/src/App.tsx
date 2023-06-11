5ba22394501f86448a6eb9d501d69a9343064634
Timofey Shichalin <shichatim@gmail.com>
2023-06-11 01:38:15 -0400

    css configured, tailwind works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import "/src/assets/css/LogicGateStyles.css";
 
 export function App() {
 	return (
 		<>
 			<p>App page</p>
 			<p>The app element is showing</p>
+			<p className="text-red-400">If this is red, tailwind is working</p>
 		</>
 	);
 }
