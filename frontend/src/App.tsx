1f5ea1b26fca200931353a3a1e415f317de49c97
Timofey Shichalin <shichatim@gmail.com>
2023-06-11 03:11:10 -0400

    added navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import "/src/assets/css/LogicGateStyles.css";
+import React from "react";
+import Navbar from "@/components/Navbar.tsx";
 
 export function App() {
 	return (
 		<>
+			<Navbar></Navbar>
 			<p>App page</p>
 			<p>The app element is showing</p>
 			<p className="text-red-400">If this is red, tailwind is working</p>
