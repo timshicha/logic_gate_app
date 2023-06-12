2526955b680ec5ce9da4fd2f0074a909e778f686
Timofey Shichalin <shichatim@gmail.com>
2023-06-12 15:48:06 -0400

    place wires on canvas (still broken)

--- frontend/src/components/CircuitMapPage.tsx
+++ frontend/src/components/CircuitMapPage.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useRef, useState} from "react";
 import { COLORS } from "@/utils/constants.tsx";
 
+let toolInHand = "wire";
+let c = 0;
 
 export function CircuitMapPage() {
 
@@ -8,16 +10,16 @@ export function CircuitMapPage() {
 	// How many "blocks" the canvas should be. The more blocks, the
 	// bigger the grid. This allows the user to place gates and wires
 	// on a bigger board.
-	const CANVAS_UNITS = 40;
+	const CANVAS_UNITS = 10;
 	// How many pixels each "block" should be.
-	const UNIT_SIZE = 15;
+	const UNIT_SIZE = 70;
 	const mainCanvasRef = useRef(null);
 	const gridCanvasRef = useRef(null);
 	const hintCanvasRef = useRef(null);
+	const [wireStart, setWireStart] = useState(null);
 	// Where the user's last coordinates were
 	let clientX = 0;
 	let clientY = 0;
-	let toolInHand = "wire";
 
 	useEffect(() => {
 		// Add a listener to detect canvas clicks
@@ -29,6 +31,10 @@ export function CircuitMapPage() {
 			mainCanvasRef.current.removeEventListener("mousemove", event => handleCanvasMove(event));
 		}
 	});
+
+	function clearCanvas(canvas) {
+		canvas.getContext("2d").clearRect(0, 0, gridCanvasRef.current.width, gridCanvasRef.current.height);
+	}
 
 	// Given two coordinates on the canvas, find the nearest grid intersection and
 	// return its x and y.
@@ -48,29 +54,53 @@ export function CircuitMapPage() {
 		const coordY = event.clientY - rect.top;
 
 		let [x, y] = readCanvasClick(coordX, coordY);
-		console.log(x, y);
 
-		// If placement is not wire, then place where the user clicks
-		if(toolInHand !== "wire") {
+		// If a wire is in hand, special treatment
+		if(toolInHand === "wire") {
+			// If no first point selected, select this as first point
+			if(wireStart === null) {
+				setWireStart([x, y]);
+			}
+			// If selected the same point, cancel wire placement
+			else if (wireStart[0] === x && wireStart[1] === y) {
+				setWireStart(null);
+				hintCanvasRef.current.getContext("2d").clearRect(0, 0, mainCanvasRef.current.width, mainCanvasRef.current.height);
+			}
+			// Otherwise, place the wire
+			else {
+				draw(mainCanvasRef.current, toolInHand, wireStart[0], wireStart[1], x, y, COLORS.BLACK);
+				setWireStart(null);
+				hintCanvasRef.current.getContext("2d").clearRect(0, 0, mainCanvasRef.current.width, mainCanvasRef.current.height);
+			}
+		}
+		else {
 			// Clear hint canvas
-			hintCanvasRef.current.getContext("2d").clearRect(0, 0, mainCanvasRef.current.width, mainCanvasRef.current.height);
 			draw(mainCanvasRef.current, toolInHand, x,y, null, null, COLORS.BLACK);
+			hintCanvasRef.current.getContext("2d").clearRect(0, 0, mainCanvasRef.current.width, mainCanvasRef.current.height);
 		}
 	}
 
 	// When the user moves in the canvas, see if they hovered over a new grid intersection.
 	// If so, update the canvas.
 	function handleCanvasMove(event) {
-		console.log("tool", toolInHand);
 		const rect = hintCanvasRef.current.getBoundingClientRect();
 		const coordX = event.clientX - rect.left;
 		const coordY = event.clientY - rect.top;
 		let [x, y] = readCanvasClick(coordX, coordY);
 		if(x !== clientX || y !== clientY) {
-			hintCanvasRef.current.getContext("2d").clearRect(0, 0, mainCanvasRef.current.width, mainCanvasRef.current.height);
-			draw(hintCanvasRef.current, toolInHand, x, y, null, null, COLORS.DARK_GRAY);
 			clientX = x;
 			clientY = y;
+			console.log("new x and y:", x, y);
+			hintCanvasRef.current.getContext("2d").clearRect(0, 0, mainCanvasRef.current.width, mainCanvasRef.current.height);
+			// Wires get special treatment
+			if(toolInHand === "wire") {
+				if(wireStart) {
+					draw(hintCanvasRef.current, toolInHand, wireStart[0], wireStart[1], x, y, COLORS.DARK_GRAY);
+				}
+			}
+			else {
+				draw(hintCanvasRef.current, toolInHand, x, y, null, null, COLORS.DARK_GRAY);
+			}
 		}
 	}
 
@@ -97,8 +127,6 @@ export function CircuitMapPage() {
 	// Draw the specified shape.
 	// If wire, x2 and y2 must also be specified.
 	function draw(canvas, obj, x1, y1, x2, y2, color) {
-
-		console.log("Drawing", obj);
 		const context = canvas.getContext("2d");
 		function strokeAndFill () {
 			context.strokeStyle = color;
@@ -106,6 +134,7 @@ export function CircuitMapPage() {
 			context.lineWidth = 1;
 			context.stroke();
 			context.fill();
+			context.closePath();
 		}
 
 		context.beginPath();
@@ -126,23 +155,25 @@ export function CircuitMapPage() {
 			context.moveTo((x1 + 0.5) * UNIT_SIZE, y1 * UNIT_SIZE);
 			context.lineTo((x1 - 1) * UNIT_SIZE, (y1 - 1) * UNIT_SIZE);
 			context.lineTo((x1 - 1) * UNIT_SIZE, (y1 + 1) * UNIT_SIZE);
-			context.lineTo((x1 + 0.5) * UNIT_SIZE, y1 * UNIT_SIZE);
-			context.arc((x1 + 0.75) * UNIT_SIZE, y1 * UNIT_SIZE, 0.25 * UNIT_SIZE, Math.PI, 3 * Math.PI, false);
+			context.lineTo((x1 + 1/3) * UNIT_SIZE, y1 * UNIT_SIZE);
+			context.arc((x1 + 2/3) * UNIT_SIZE, y1 * UNIT_SIZE, 1/3 * UNIT_SIZE, Math.PI, 4 * Math.PI, false);
 			strokeAndFill();
 		}
 		else if(obj === "wire") {
 			context.moveTo(x1 * UNIT_SIZE, y1 * UNIT_SIZE);
-			context.arc(x1 * UNIT_SIZE, y1 * UNIT_SIZE, UNIT_SIZE / 4, 0, 2 * Math.PI);
+			context.arc(x1 * UNIT_SIZE, y1 * UNIT_SIZE, 0.125 * UNIT_SIZE, 0, 2 * Math.PI);
 			strokeAndFill();
 			context.beginPath();
 			context.moveTo(x1 * UNIT_SIZE, y1 * UNIT_SIZE);
 			context.lineTo(x2 * UNIT_SIZE, y2 * UNIT_SIZE);
 			strokeAndFill();
 			context.beginPath();
-			context.arc(x2 * UNIT_SIZE, y2 * UNIT_SIZE, UNIT_SIZE / 4, 0, 2 * Math.PI);
+			context.arc(x2 * UNIT_SIZE, y2 * UNIT_SIZE, 0.125 * UNIT_SIZE, 0, 2 * Math.PI);
 			context.lineTo(x2 * UNIT_SIZE, y2 * UNIT_SIZE);
+			console.log(c++);
 			strokeAndFill();
 		}
+		context.closePath();
 	}
 
 
@@ -151,6 +182,7 @@ export function CircuitMapPage() {
 			Circuit Map Page
 			<br />
 			<button onClick={() => drawCanvasGridLines(COLORS.GRAY)}>Set up</button>
+			<button onClick={() => {clearCanvas(mainCanvasRef.current)}}>Clear canvas</button>
 			<br />
 
 			<div>
