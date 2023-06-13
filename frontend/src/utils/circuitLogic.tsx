1b839dcf2f913f9da078cbf1b39b124cd485e6d1
Timofey Shichalin <shichatim@gmail.com>
2023-06-13 00:45:46 -0400

    added method to CircuitBoard class to remove objects

--- frontend/src/utils/circuitLogic.tsx
+++ frontend/src/utils/circuitLogic.tsx
@@ -42,4 +42,30 @@ class CircuitBoard {
             console.log("Class CircuitBoard: invalid object passed to addObject:", obj);
         }
     }
+
+    // Clears a grid spot: wires that start/end and objects centered on this grid spot
+    // will be removed;
+    public clearSelection(x, y) {
+        // Find wires that start/end here
+        for (let i = this.wirePairs.length; i >= 0; i--) {
+            // If wire start or end matches
+            if((this.wirePairs[i][0] === x && this.wirePairs[i][1] === y) ||
+                (this.wirePairs[i][2] === x && this.wirePairs[i][3] === y)){
+                this.wirePairs.splice(i, 1);
+            }
+        }
+        // Find objects that are centered here
+        function removeMatches(array, x, y) {
+            for (let i = array.length; i >= 0; i--) {
+                if(array[i][0] === x &&array[i][1] === y) {
+                    array.splice(i, 1);
+                }
+            }
+        }
+        removeMatches(this.andGates, x, y);
+        removeMatches(this.orGates, x, y);
+        removeMatches(this.notGates, x, y);
+        removeMatches(this.inputs, x, y);
+        removeMatches(this.lights, x, y);
+    }
 }
