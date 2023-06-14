e3f9eeba8addbedaacecbc05ad760aaffa6ac986
Timofey Shichalin <shichatim@gmail.com>
2023-06-14 15:03:46 -0400

    made login page nice

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -5,10 +5,11 @@ export function LoginPage() {
 	
 	const [navigateToMaps, setNavigateToMaps] = useState(localStorage.getItem("email"));
 	
-	function login () {
-		const email = document.getElementById("email").value;
+	function login (event) {
+		event.preventDefault();
+		const email = event.target.email.value;
+		console.log(email);
 		localStorage.setItem("email", email);
-		document.getElementById("email").value;
 		window.location.reload();
 	}
 	
@@ -17,11 +18,12 @@ export function LoginPage() {
 			{navigateToMaps &&
 				<Navigate to="/mymaps" />
 			}
-			Login Page
-			< br/>
-			Email: <input type="email" id="email"/>
-			<br />
-			<button onClick={login}>login</button>
+			<form onSubmit={login} className={"w-fit bg-gray-200 p-[20px] rounded-xl mx-auto mt-[20px]"}>
+				<h1 className="text-[50px] block text-center">Login</h1>
+				<label htmlFor={"loginEmail"} className={"text-[20px]"}>Email: </label>
+				<input type="email" id="loginEmail" name="email" className={"w-[400px] h-[40px] m-[5px] bg-gray-600 rounded-lg text-lg align-middle"}/>
+				<button type={"submit"} className={"block w-[200px] h-[40px] m-[15px] mx-auto bg-gray-500 rounded-lg text-lg"}>Login</button>
+			</form>
 		</>
 	);
 }
