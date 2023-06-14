cd95cb9a9c181d525432f8ebb5056f6a17225be2
Timofey Shichalin <shichatim@gmail.com>
2023-06-14 15:18:17 -0400

    made home page look nicer

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import circuit_img from "@/assets/images/circuit.svg";
 
 export function HomePage() {
 	return (
 		<>
-			Home Page
+			<h1 className={"block text-[50px] m-[20px] text-center"}>Welcome to Logic Page Simulator</h1>
+			<hr className={"border-[5px]"}/>
+			
+			<img src={circuit_img} className={"w-full my-[30px]"}/>
+			<p className={"block w-3/4 text-[30px] mt-[30px] mx-auto"}>
+				Have you ever wanted to build and simulate a logic circuits? Well,
+				you have come to the right place. Have fun!
+			</p>
+			<img src={circuit_img} className={"w-full my-[40px]"}/>
 		</>
 	);
 }
